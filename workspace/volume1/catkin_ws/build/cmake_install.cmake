f3439a1a249fb664cbab45aac9a97043eb76f3c5
Pedro Jullian Graça <pedrojullian@poli.ufrj.br>
2022-09-13 15:54:29 -0300

    -

--- workspace/volume1/catkin_ws/build/cmake_install.cmake
+++ workspace/volume1/catkin_ws/build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/volume/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/volume/catkin_ws/build/pkg-gracinha2/cmake_install.cmake")
 
 endif()
 
---
+++ workspace/volume1/catkin_ws/build/pkg-gracinha2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/volume/catkin_ws/src/pkg-gracinha2
+# Build directory: /home/volume/catkin_ws/build/pkg-gracinha2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
