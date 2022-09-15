ebb2c0be1c17ccaeb823ba64da5f80d916c6e049
Pedro Graça <pedrojullian@poli.ufrj.br>
2022-09-15 11:22:36 -0300

    .

--- docker/init.sh
+++ docker/init.sh
@@ -6,7 +6,5 @@ xhost +local:docker
 mkdir -p /run/user/1000/snap.pycharm-community
 chmod 0700 /run/user/1000/snap.pycharm-community
 
-
-
 docker run -it -v $local/workspace/volume1/:/home/volume -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/lyonn/.Xauthority -h $HOSTNAME -e DISPLAY=${DISPLAY} -e XDG_RUNTIME_DIR={XDG_RUNTIME_DIR} ros1.0
 
