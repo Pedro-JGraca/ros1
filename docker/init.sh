c832e74530ccff7e97faa364e25c59a1f657bcce
Pedro Jullian <pedro@pop-os.localdomain>
2022-09-15 11:18:02 -0300

    ros tutorial funcionando no docker

--- docker/init.sh
+++ docker/init.sh
@@ -1,5 +1,12 @@
 sudo docker build -t ros1.0 .
 local=~/Documentos/ros1/
 
-docker run -it -v $local/workspace/volume1/:/home/volume ros1.0
+xhost +local:docker
+
+mkdir -p /run/user/1000/snap.pycharm-community
+chmod 0700 /run/user/1000/snap.pycharm-community
+
+
+
+docker run -it -v $local/workspace/volume1/:/home/volume -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/lyonn/.Xauthority -h $HOSTNAME -e DISPLAY=${DISPLAY} -e XDG_RUNTIME_DIR={XDG_RUNTIME_DIR} ros1.0
 
