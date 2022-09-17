9788aa847f0b86b1673cc06f83a0b5e525435e17
Pedro Graça <pedrojullian@poli.ufrj.br>
2022-09-17 13:09:47 -0300

    novo Dockefile

--- docker/init.sh
+++ docker/init.sh
@@ -1,8 +1,8 @@
-sudo docker build -t ros1.0 .
+versao='ros2.0'
+sudo docker build -t $versao .
 local=~/Documentos/ros1/
 
 xhost +local:docker
 
 
-docker run -it -v $local/workspace/volume1/:/home/volume -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/lyonn/.Xauthority -h $HOSTNAME -e DISPLAY=${DISPLAY} -e XDG_RUNTIME_DIR={XDG_RUNTIME_DIR} ros1.0
-
+docker run -it -v $local/workspace/volume1/:/home/volume -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/lyonn/.Xauthority -h $HOSTNAME -e DISPLAY=${DISPLAY} -e XDG_RUNTIME_DIR={XDG_RUNTIME_DIR} $versao
